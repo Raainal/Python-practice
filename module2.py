671261e3f2f543be738af9715983276ee57148a4
Raainal <amalnair242@gmail.com>
2024-12-24 13:57:27 +0530

    Update module2.py

--- module2.py
+++ module2.py
@@ -8,3 +8,8 @@
 from module1 import pi,square
 print(pi)
 print(square(4))
+
+from random import *
+for i in range(5):
+    print(random()) # random value between 1 and 0
+    print(randint(1,10000)) # random value between 1 and 10000
