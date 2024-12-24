2fc42f551d6af0dcca53e737fd1f6e90b4451b0d
Raainal <amalnair242@gmail.com>
2024-12-24 13:51:06 +0530

    Update module2.py

--- module2.py
+++ module2.py
@@ -1,5 +1,10 @@
-import module1 as mod
-# when u mak an alias( as mod ) then u can only acces the command using the alias
-print(mod.pi)
-mod.square(4)
-mod.welcome("amal","nair")
+# import module1 as mod
+# # when u make an alias( as mod ) then u can only acces the command using the alias
+# print(mod.pi)
+# mod.square(4)
+# mod.welcome("amal","nair")
+
+#to import a specific function
+from module1 import pi,square
+print(pi)
+print(square(4))
