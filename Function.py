f8e9e45e51f0c0203f5ca8ea52a8e222407fe389
Raainal <amalnair242@gmail.com>
2024-12-24 10:20:15 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -21,5 +21,10 @@ unknown arguement
 def info(fname,lname):
     print("first name is ",fname)
     print("last name is ",lname)
-info("sachin","tendulkar")
+#info("sachin","tendulkar")
 
+#positional passin in correct order
+def add(n1,n2):
+    return n1+n2
+res=add(2,3)
+print(res)
