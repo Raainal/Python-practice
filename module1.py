ff6771c35038e377cd59b9f026ad31eab9e14cf7
Raainal <amalnair242@gmail.com>
2024-12-24 13:34:03 +0530

    h

---
+++ module1.py
@@ -0,0 +1,6 @@
+def welcome(fname,lname):
+    print("first name is ",fname)
+    print("last name is ",lname)
+def square(n):
+    print(n*n)
+pi=3.14
---
+++ module2.py
@@ -0,0 +1,5 @@
+import module1
+
+print(module1.pi)
+module1.square(4)
+module1.welcome("amal","nair")
