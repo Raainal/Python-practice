0baacaa31d414d9123de54fc6952363fe34332b8
Raainal <amalnair242@gmail.com>
2024-12-27 14:11:52 +0530

    Update exception.py

--- exception.py
+++ exception.py
@@ -5,9 +5,10 @@ try:
     b=int(input("enter 2nd val "))
     c=a/b
     print(c)
-except ZeroDivisionError as message:
-    print("error zero",message)
-except ValueError as message:
-    print("error val",message)
-
+# except ZeroDivisionError as message:
+#     print("error zero",message)
+# except ValueError as message:
+#     print("error val",message)
+except (ZeroDivisionError,ValueError) as message:
+    print("error",message)
 print("continue")
