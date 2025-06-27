6f98b0a395f7e5854d9e7dcddafc9f2e0694086b
Raainal <amalnair242@gmail.com>
2025-06-28 01:08:16 +0530

    Create Linear search.py

---
+++ Data Structures/Searching/Linear search.py
@@ -0,0 +1,7 @@
+l1=[4,7,4,3,2,3,6,7,3,2,5,45564,345,323,653634,552345,234342,345,6345,34,23,6,5634,56345,23]
+n=345
+n1=0
+for idx,i in enumerate(l1):
+    if i==n:
+        print("first index of "+str(n)+" is at index ="+str(idx))
+        break
